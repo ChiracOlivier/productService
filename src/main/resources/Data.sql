8ecc8f5b32ec77f147520964da7bf0c5e82f6149
Chiracolivier <chiracolivier28@gmail.com>
2019-10-03 23:32:34 +0200

    add @Postmapping in ProductController

--- src/main/resources/Data.sql
+++ src/main/resources/Data.sql
@@ -1,3 +1,3 @@
-insert into PRODUCT values (1,'sal', 10000)
-insert into PRODUCT values (2,'sal1', 1000)
-insert into PRODUCT values (3,'sal3', 100)
+insert into Product values (1,'sal', 10000)
+insert into Product values (2,'sal1', 1000)
+insert into Product values (3,'sal3', 100)
