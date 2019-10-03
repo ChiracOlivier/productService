c30d465811b7b3971d732f80168e4589a8828b7f
Chiracolivier <chiracolivier28@gmail.com>
2019-10-03 21:18:59 +0200

    arrange getProduct Method in Product Class in entities

--- src/main/java/com/mycompany/productService/controller/ProductController.java
+++ src/main/java/com/mycompany/productService/controller/ProductController.java
@@ -5,6 +5,8 @@ import com.mycompany.productService.entities.Product;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class ProductController {
 
@@ -15,8 +17,15 @@ public class ProductController {
     }
      // Jackson turns POJO's in JSON
     @GetMapping("/Products")
-    public Product getProduct(){
-       Product myProduct= new Product(1,"salut", 100);
-       return myProduct;
+    public List<Product> getProduct(){
+       Product myProduct= new Product(1,"salu", 10);
+        Product myProducts= new Product(2,"slut", 1000);
+        Product myProduc= new Product(3,"salut", 100);
+        List<Product> Products = null;
+        Products.add(myProduct);
+        Products.add(myProducts);
+        Products.add(myProduc);
+
+       return Products;
     }
 }
