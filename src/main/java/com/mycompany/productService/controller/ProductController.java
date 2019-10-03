e2ad77c8a187e270479e03d445bde568e351da20
Chiracolivier <chiracolivier28@gmail.com>
2019-10-03 21:09:01 +0200

    add getProduct Method in Product Class in entities

--- src/main/java/com/mycompany/productService/controller/ProductController.java
+++ src/main/java/com/mycompany/productService/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.mycompany.productService.controller;
 
 
+import com.mycompany.productService.entities.Product;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +13,10 @@ public class ProductController {
     public String TakeProduct(){
         return "Welcome to me Chirac";
     }
-
+     // Jackson turns POJO's in JSON
     @GetMapping("/Products")
     public Product getProduct(){
-
+       Product myProduct= new Product(1,"salut", 100);
+       return myProduct;
     }
 }
--- src/main/java/com/mycompany/productService/entities/Product.java
+++ src/main/java/com/mycompany/productService/entities/Product.java
@@ -1,4 +1,22 @@
 package com.mycompany.productService.entities;
 
+import lombok.Data;
+
+@Data
 public class Product {
+
+    private Integer id;
+    private String name;
+    private int price;
+
+    public Product(){
+
+    }
+
+    public Product(Integer id, String name, int price) {
+        super();
+        this.id = id;
+        this.name=name;
+        this.price=price;
+    }
 }
